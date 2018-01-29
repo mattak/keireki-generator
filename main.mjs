040963c4e6c27c82a2c72a9e04d2250eb744225e
mattak <mattak.me@gmail.com>
2018-01-29 19:32:00 +0900

    Convert from html to pdf

---
+++ main.mjs
@@ -0,0 +1,25 @@
+import fs from 'fs';
+import pdf from 'html-pdf';
+
+if (process.argv.length <= 2) {
+  console.log("Usage: <input_html> <output_pdf>?");
+  process.exit(1);
+}
+
+if (!process.argv[2].match(/\.html$/)) {
+  console.log("Error: Input file must be html file.");
+  process.exit(1);
+}
+
+var infile  = process.argv[2];
+var outfile = (process.argv.length > 3) ? process.argv[3] : infile.replace(/\.html$/,".pdf");
+
+var html = fs.readFileSync(infile, 'utf8');
+var options = { format: 'Letter' };
+
+pdf.create(html, options)
+  .toFile(outfile, (err, res) => {
+    if (err) return console.log(err);
+    console.log(res);
+  });
+
